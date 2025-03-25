16de7f270ad5d2f1473c857fbd69924d5816049c
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2025-03-25 10:47:34 -0300

    first commit

---
+++ src/test/java/br/com/ntconsult/rest_api_books/RestApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ntconsult.rest_api_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
