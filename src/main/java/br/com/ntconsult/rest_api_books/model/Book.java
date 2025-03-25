b2d88f58074e1b4547ec218df4adf0819ab891fc
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2025-03-25 11:09:54 -0300

    created model Book

---
+++ src/main/java/br/com/ntconsult/rest_api_books/model/Book.java
@@ -0,0 +1,56 @@
+package br.com.ntconsult.rest_api_books.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    @Column(name = "publish_date")
+    private LocalDate publishDate;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+}
